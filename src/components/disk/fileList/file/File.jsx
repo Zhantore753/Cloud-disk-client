6cc85e28992c4ec6fd5ed8a986b2638d110f2dba
Zhantore <753gg@mail.ru>
2021-04-15 11:37:39 +0600

    Загрузка файлов. Отслеживание прогресса

--- src/components/disk/fileList/file/File.jsx
+++ src/components/disk/fileList/file/File.jsx
@@ -9,12 +9,14 @@ const File = ({file}) => {
     const currentDir = useSelector(state => state.files.currentDir);
 
     function openDirHandler() {
-        dispatch(pushToStack(currentDir));
-        dispatch(setCurrentDir(file._id));
+        if(file.type === 'dir') {
+            dispatch(pushToStack(currentDir))
+            dispatch(setCurrentDir(file._id))
+        }
     }
 
     return (
-        <div className='file' onClick={file.type === 'dir' ? ()=> openDirHandler() : ''}>
+        <div className='file' onClick={()=> openDirHandler()}>
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0,10)}</div>
